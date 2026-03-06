ba73fd51f1e2b3a245fe6c4ada35aab8c6d40d8b
Ayush Choudhary <ayushchoudhary23923@gmail.com>
2026-03-06 09:45:51 +0530

    project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,10 +10,7 @@ const Projects = () => {
           title="Portfolio Website"
           main="A personal portfolio to demonstrate my frontend development skills and projects with responsive and interactive designs using React.js and Tailwind CSS."
         />
-        <ProjectCard
-          title="Video Conference Web"
-          main="Built the frontend of a video conferencing web app with React.js and CSS, featuring responsive design, interactive UI, and real-time user experience."
-        />
+        
         
       </div>
     </div>
